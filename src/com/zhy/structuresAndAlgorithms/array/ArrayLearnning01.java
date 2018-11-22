42c189f0f2af4dcbf9e08e3ccc5637b808d5ee5f
zhy <zycool0828@163.com>
2018-11-22 20:03:21 +0800

    Initial commit

---
+++ src/com/zhy/structuresAndAlgorithms/array/ArrayLearnning01.java
@@ -0,0 +1,23 @@
+package com.zhy.structuresAndAlgorithms.array;
+
+public class ArrayLearnning01 {
+    public static void main(String[] args) {
+        int[][] arrayss = new int[10][3];
+        int[] arrays1 = new int[3];
+        int[] arrays2 = new int[3];
+        int[] arrays3 = new int[3];
+        int[] arrays4 = new int[3];
+        int[] arrays5 = new int[3];
+        int[] arrays6 = new int[3];
+        int[] arrays7 = new int[3];
+        int[] arrays8 = new int[3];
+        int[] arrays9 = new int[3];
+        int[] arrays10 = new int[3];
+
+        arrayss[0] = arrays1;
+        arrayss[1] = arrays2;
+        arrayss[2] = arrays3;
+        arrayss[3] = arrays4;
+        arrayss[4] = arrays5;
+    }
+}
---
+++ src/com/zhy/structuresAndAlgorithms/array/ArrayLearnning02.java
@@ -0,0 +1,94 @@
+package com.zhy.structuresAndAlgorithms.array;
+
+public class ArrayLearnning02 {
+    //定义一个数组 进行保存数据
+    private int[] data;
+    //定义数组长度
+    private int size;
+    //定义数组中实际的个数：
+    private int count;
+
+    //构造方法 需要定义数组的大小
+    public ArrayLearnning02(int size) {
+        this.data = new int[size];
+        this.size = size;
+        this.count = 0;//刚开始数组的大小为0
+    }
+
+    /**
+     * 根据数组索引进行查询
+     * @return
+     */
+    public int find(int index){
+        if(index<0||index>size)return -1;
+        return data[index];
+    }
+
+    /**
+     * 插入元素:头部插入 尾部插入
+     * @return
+     */
+    public boolean insert(int index,int value){
+        //数组已经满了
+        if(count==size){
+            System.out.println("数组已经满了...");
+            return false;
+        }
+
+        //index的校验
+
+        if(index<0||index>size){
+            System.out.println("位置不合适");
+            return false;
+        }
+
+        //位置合法 进行插入
+        //index之后的数据 向后移一个位
+        for (int i = count;i>index;i--){
+            data[i] = data[i-1];
+        }
+        //插入数据
+        data[index] = value;
+        count++;
+        return true;
+    }
+
+    /**
+     * 根据下标进行删除
+     * @param index
+     * @return
+     */
+    public boolean delete(int index){
+        if (index<0||index>count){
+            System.out.println("索引错误");
+            return false;
+        }
+
+        //从删除位置之后的数据 向前移动一个位置
+        for (int i =index+1;i<count;i++){
+            data[i-1] = data[i];
+        }
+        --count;
+        return true;
+    }
+
+
+    public void printAll() {
+        for (int i = 0; i < count; ++i) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ArrayLearnning02 array = new ArrayLearnning02(5);
+        array.printAll();
+        array.insert(0, 3);
+        array.insert(0, 4);
+        array.insert(1, 5);
+        array.insert(3, 9);
+        array.insert(3, 10);
+        //array.insert(3, 11);
+        array.printAll();
+    }
+}
